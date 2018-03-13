080e736d9b236ac10aa6760292e25ea4fe62d722
buns-li <buns.zpli@gmail.com>
2018-03-13 17:37:34 +0800

    调整代码排版

--- index.js
+++ index.js
@@ -1,39 +1,42 @@
-'use strict'
+"use strict";
 
-const path = require('path')
-const nodemailer = require('nodemailer')
-const bcore = require('bcore')
+const path = require("path");
+const nodemailer = require("nodemailer");
+const bcore = require("bcore");
 
-const slice = Array.prototype.slice
+const slice = Array.prototype.slice;
 
 const engineMap = {
-    '.njk': 'nunjucks',
-    '.html': 'nunjucks',
-    '.pug': 'jade',
-    'jade': 'jade',
-    '.dot': 'dot',
-    '.jst': 'dot',
-    '.art': 'artTemplate',
-    '.ejs': 'ejs',
-    '.hbs': 'hbs',
-    '.md': 'markdown',
-    '.markdown': 'markdown'
-}
+  ".njk": "nunjucks",
+  ".html": "nunjucks",
+  ".pug": "jade",
+  ".jade": "jade",
+  ".dot": "dot",
+  ".jst": "dot",
+  ".art": "artTemplate",
+  ".ejs": "ejs",
+  ".hbs": "hbs",
+  ".md": "markdown",
+  ".markdown": "markdown"
+};
 
-const confSymbol = Symbol['email#conf']
+const confSymbol = Symbol["email#conf"];
 
-bcore.on('email', {
+bcore.on(
+  "email",
+  {
     //发送回邮件的附件根目录
     attachRoot: process.cwd(),
-    from: '',
+    from: "",
     auth: {},
-    host: 'smtp.126.com',
+    host: "smtp.126.com",
     port: 465,
     secure: true, //
     pool: false,
     debug: false,
     logger: false
-}, function() {
+  },
+  function() {
     /**
      * 构建发送邮件的邮件配置项
      *
@@ -48,51 +51,49 @@ bcore.on('email', {
      * @returns {Object} 邮件发送配置项
      */
     function createMailOptions(...args) {
+      let opts = { subject: args[1] };
 
-        let opts = { subject: args[1] }
+      args[4] && (opts.attachments = args[4]);
 
-        args[4] && (opts.attachments = args[4])
+      if (typeof args[2] === "string") {
+        opts.to = args[2];
+      } else if (typeof args[2] === "object") {
+        opts.to = args[2].to;
+        args[2].cc && (opts.cc = args[2].cc);
+        args[2].bcc && (opts.bcc = args[2].bcc);
+      }
 
-        if (typeof args[2] === 'string') {
-            opts.to = args[2]
-        } else if (typeof args[2] === 'object') {
-            opts.to = args[2].to
-            args[2].cc && (opts.cc = args[2].cc)
-            args[2].bcc && (opts.bcc = args[2].bcc)
-        }
+      switch (args[0]) {
+        case "html":
+          opts.html = args[3];
+          break;
+        case "plain":
+        default:
+          opts.text = args[3];
+          break;
+      }
 
-        switch (args[0]) {
-            case 'html':
-                opts.html = args[3]
-                break
-            case 'plain':
-            default:
-                opts.text = args[3]
-                break
-        }
-
-        return opts
+      return opts;
     }
 
     this.__init = function(options) {
+      let conf = (this[confSymbol] = {});
 
-        let conf = this[confSymbol] = {}
+      options.attachRoot &&
+        (conf.attachRoot = path.normalize(options.attachRoot));
 
-        options.attachRoot && (conf.attachRoot = path.normalize(options.attachRoot))
+      options.from && (conf.from = options.from);
 
-        options.from && (conf.from = options.from)
-
-        conf.transport = nodemailer.createTransport({
-            host: options.host,
-            port: options.port,
-            secure: !!options.secure, //强制转换成Boolean类型,
-            auth: options.auth,
-            pool: !!options.pool,
-            debug: !!options.debug,
-            logger: options.logger
-        })
-    }
-
+      conf.transport = nodemailer.createTransport({
+        host: options.host,
+        port: options.port,
+        secure: !!options.secure, //强制转换成Boolean类型,
+        auth: options.auth,
+        pool: !!options.pool,
+        debug: !!options.debug,
+        logger: options.logger
+      });
+    };
 
     /**
      * 发送普通文本
@@ -112,23 +113,24 @@ bcore.on('email', {
      * @return {Promise<any>}
      */
     this.sendPlain = function(subject, reciever, text, attachments) {
+      let conf = this[confSymbol];
 
-        let conf = this[confSymbol]
+      let args = slice.call(arguments);
 
-        let args = slice.call(arguments)
+      args.unshift("plain");
 
-        args.unshift('plain')
+      let options = createMailOptions(...args);
 
-        let options = createMailOptions(...args)
+      options.from = conf.from;
 
-        options.from = conf.from
+      attachments &&
+        !Array.isArray(attachments) &&
+        (attachments = [attachments]);
 
-        attachments && !Array.isArray(attachments) && (attachments = [attachments])
+      attachments && attachments.length && (options.attachments = attachments);
 
-        attachments && attachments.length && (options.attachments = attachments)
-
-        return conf.transport.sendMail(options)
-    }
+      return conf.transport.sendMail(options);
+    };
 
     /**
      * 发送html内容
@@ -148,23 +150,24 @@ bcore.on('email', {
      * @return {Promise<any>}
      */
     this.sendHtml = function(subject, reciever, htmlStr, attachments) {
+      let conf = this[confSymbol];
 
-        let conf = this[confSymbol]
+      let args = slice.call(arguments);
 
-        let args = slice.call(arguments)
+      args.unshift("html");
 
-        args.unshift('html')
+      let options = createMailOptions(...args);
 
-        let options = createMailOptions(...args)
+      options.from = conf.from;
 
-        options.from = conf.from
+      attachments &&
+        !Array.isArray(attachments) &&
+        (attachments = [attachments]);
 
-        attachments && !Array.isArray(attachments) && (attachments = [attachments])
+      attachments && attachments.length && (options.attachments = attachments);
 
-        attachments && attachments.length && (options.attachments = attachments)
-
-        return conf.transport.sendMail(options)
-    }
+      return conf.transport.sendMail(options);
+    };
 
     /**
      * 发送带模板引擎处理的html文件内容
@@ -184,26 +187,34 @@ bcore.on('email', {
      *
      * @return {Promise<any>}
      */
-    this.sendTplHtml = async function(subject, reciever, fileUrl, tplContext, attachments) {
+    this.sendTplHtml = async function(
+      subject,
+      reciever,
+      fileUrl,
+      tplContext,
+      attachments
+    ) {
+      let conf = this[confSymbol];
 
-        let conf = this[confSymbol]
+      let extname = path.extname(fileUrl);
 
-        let extname = path.extname(fileUrl)
+      let renderer = require("./lib/" + engineMap[extname] || "markdown");
 
-        let renderer = require('./lib/' + engineMap[extname] || 'markdown')
+      let parsedHtml = await renderer.render(fileUrl, tplContext);
 
-        let parsedHtml = await renderer.render(fileUrl, tplContext)
+      let args = ["html", subject, reciever, parsedHtml, attachments];
 
-        let args = ['html', subject, reciever, parsedHtml, attachments]
+      let options = createMailOptions(...args);
 
-        let options = createMailOptions(...args)
+      options.from = conf.from;
 
-        options.from = conf.from
+      attachments &&
+        !Array.isArray(attachments) &&
+        (attachments = [attachments]);
 
-        attachments && !Array.isArray(attachments) && (attachments = [attachments])
+      attachments && attachments.length && (options.attachments = attachments);
 
-        attachments && attachments.length && (options.attachments = attachments)
-
-        return conf.transport.sendMail(options)
-    }
-})
+      return conf.transport.sendMail(options);
+    };
+  }
+);
